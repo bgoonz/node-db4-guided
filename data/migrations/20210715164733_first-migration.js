5a996974576c2c2d66284eac061a291afa65d07b
bryan <bryan.guner@gmail.com>
2021-07-15 13:03:15 -0400

    update

---
+++ data/migrations/20210715164733_first-migration.js
@@ -0,0 +1,27 @@
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('zoos', table => {
+      table.increments('zoo_id')
+      table.string('zoo_name', 250).notNullable().unique()
+      table.string('zoo_address', 500).notNullable()
+    })
+    .createTable('species', table => {
+      table.increments('species_id')
+      table.string('species_name', 300).notNullable().unique()
+    })
+    .createTable('animals', table => {
+      table.increments('animals_id')
+      table.string('animal_name', 100).notNullable()
+    })
+    .createTable('zoo_animals', table => {
+      table.increments('zoo_animal_id')
+    })
+};
+
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('zoo_animals')
+    .dropTableIfExists('animals')
+    .dropTableIfExists('species')
+    .dropTableIfExists('zoos')
+};
